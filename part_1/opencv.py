db1a6c813af0e9328b725475b39798a1ed2281d5
ded-evsey <39158683+ded-evsey@users.noreply.github.com>
2020-03-09 18:33:06 +0400

    complite opencv

--- part_1/opencv.py
+++ part_1/opencv.py
@@ -2,15 +2,31 @@ import cv2
 import numpy as np
 import os
 
-FILE_NAME = 'kitten'
-KERNEL = np.ones((5, 5), np.uint8)
+# Input images
+FILE_NAME_KITTEN = 'kitten'
+FILE_NAME_BOOK = 'book'
+FILE_NAME_GEOMETRY = 'geometry'
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+# Support variables
+CLEAR_IMG = lambda w, h: np.zeros(shape=[w, h, 3], dtype=np.uint8)
+KERNEL = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+HSV_MIN = np.array((2, 28, 65), np.uint8)
+HSV_MAX = np.array((26, 238, 255), np.uint8)
 
 
-def read_write_file(grey=False, file_name=FILE_NAME):
+def read_write_file(func_name, grey, file_name):
     """
     Декоратор, создающий объект изображения в памяти
     и сохраняющий изображение после выполнения функции.
     Так же, при необходимости переводит изображение в серый
+    :param func_name
     :param grey: bool
     :param file_name: str
     """
@@ -21,13 +37,12 @@ def read_write_file(grey=False, file_name=FILE_NAME):
 
         def wrapper(image=img):
             return_img = func(image)
-            cv2.imwrite(f'output_files/{file_name}/{func.__name__}.jpg', return_img)
-            print(f'{func.__name__} complete')
+            cv2.imwrite(f'output_files/{file_name}/{func_name}.jpg', return_img)
+            print(f'{func_name} complete')
         return wrapper
     return args_wrapper
 
 
-@read_write_file(grey=True)
 def laplacian(img):
     """
     Загрузить заданное изображение из файла.
@@ -40,7 +55,6 @@ def laplacian(img):
     return cv2.Laplacian(img, cv2.CV_16S)
 
 
-@read_write_file()
 def blur_gaussian(img):
     """
     Загрузить изображение из файла.
@@ -52,11 +66,10 @@ def blur_gaussian(img):
     return cv2.GaussianBlur(img, (5, 5), cv2.BORDER_DEFAULT)
 
 
-@read_write_file()
 def median(img):
     """
     Загрузить изображение из файла.
-    Применить сглаживающий фильтр Гаусса.
+    Применить медианный сглаживающий фильтр.
     Результат сохранить в файл.
     :param img obj
     :return: img obj post correct
@@ -64,11 +77,17 @@ def median(img):
     return cv2.medianBlur(img, 1)
 
 
-def morph(img, type_morph, kernel=KERNEL, iterations=1):
-    return cv2.morphologyEx(img, type_morph, kernel, iterations=iterations)
+def morph(img, type_morph, kernel=KERNEL):
+    """
+    Функция, для сокращения вызова записи морфологического преобразования
+    :param img: объект изображения
+    :param type_morph: тип преобразования
+    :param kernel: кернел
+    :return:
+    """
+    return cv2.morphologyEx(img, type_morph, kernel)
 
 
-@read_write_file(grey=True)
 def top_hat(img):
     """
     Загрузить изображение из файла.
@@ -82,7 +101,6 @@ def top_hat(img):
     return morph(img, cv2.MORPH_TOPHAT)
 
 
-@read_write_file(grey=True)
 def gradient(img):
     """
     Загрузить изображение из файла.
@@ -96,7 +114,6 @@ def gradient(img):
     return morph(img, cv2.MORPH_GRADIENT)
 
 
-@read_write_file(grey=True)
 def closing(img):
     """
     Загрузить изображение из файла.
@@ -110,7 +127,6 @@ def closing(img):
     return morph(img, cv2.MORPH_CLOSE)
 
 
-@read_write_file(grey=True)
 def erosion(img):
     """
     Загрузить изображение из файла.
@@ -124,7 +140,6 @@ def erosion(img):
     return cv2.erode(img, KERNEL)
 
 
-@read_write_file()
 def rotate(img):
     """
     Загрузить изображение из файла.
@@ -139,7 +154,6 @@ def rotate(img):
     return cv2.warpAffine(img, M, (w, h))
 
 
-@read_write_file()
 def move(img):
     """
     Загрузить изображение из файла.
@@ -153,18 +167,173 @@ def move(img):
     return cv2.warpAffine(img, M, (cols, rows))
 
 
+def canny(img):
+    """
+    Загрузить изображение из файла и преобразовать
+    к оттенкам серого.
+    Преобразовать изображение фильтром Канни.
+    Результат сохранить в файл
+    :param img:
+    :return:
+    """
+    return cv2.Canny(img, 10, 250)
+
+
+def contour(img):
+    """
+    Загрузить изображение из файла.
+    Определить контуры.
+    Сохранить их изображения в отдельный файл
+    :param img:
+    :return:
+    """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # меняем цветовую модель с BGR на HSV
+    thresh = cv2.inRange(hsv, HSV_MIN, HSV_MAX)  # применяем цветовой фильтр
+    contours, hierarchy = cv2.findContours(
+        thresh.copy(),
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_TC89_KCOS
+    )
+    (h, w) = img.shape[:2]
+    return cv2.drawContours(
+        CLEAR_IMG(h, w),
+        contours,
+        -1,
+        BLACK
+    )
+
+
+def search_geometry(img):
+    """
+    Предполагая наличие на
+    фотоснимке обложки книги
+    обвести соответствующую
+    часть изображения прямоугольником.
+    Результат вывести на экран.
+    :param img:
+    :return img:
+    """
+    blur = blur_gaussian(img)
+    dirt_contour = canny(blur)
+    closing_contour = closing(dirt_contour)
+    cntr, hier = cv2.findContours(closing_contour.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    for cont in cntr:
+        peri = cv2.arcLength(cont, True)
+        approx = cv2.approxPolyDP(cont, 0.045 * peri, True)
+        if len(approx) == 4:
+            img = cv2.drawContours(img, [approx], -1, GREEN, 4)
+        if len(approx) == 3:
+            img = cv2.drawContours(img, [approx], -1, RED, 3)
+    return img
+
+
+def equalize_hist(img):
+    """
+    Для заданного изображения выполнить
+    операцию выравнивания гистограммы.
+    Вывести на экран исходное
+    и результирующие изображения #59
+    :param img:
+    :return:
+    """
+    equ = cv2.equalizeHist(img)
+    return np.hstack((img, equ))
+
+
 if __name__ == '__main__':
-    if not os.path.exists(f'output_files/{FILE_NAME}'):
-        os.mkdir(f'output_files/{FILE_NAME}')
-    # filters
-    laplacian()
-    blur_gaussian()
-    median()
-    # morphology
-    top_hat()
-    gradient()
-    closing()
-    erosion()
-    # geometric
-    rotate()
-    move()
+    for FILE_NAME in [FILE_NAME_KITTEN, FILE_NAME_BOOK, FILE_NAME_GEOMETRY]:
+        if not os.path.exists(f'output_files/{FILE_NAME}'):
+            os.mkdir(f'output_files/{FILE_NAME}')
+    functions = [
+        {
+            'func': canny,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': canny.__name__
+        },
+        {
+            'func': erosion,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': erosion.__name__
+        },
+        {
+            'func': laplacian,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': laplacian.__name__
+        },
+        {
+            'func': closing,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': closing.__name__
+        },
+        {
+            'func': top_hat,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': top_hat.__name__
+        },
+        {
+            'func': gradient,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': gradient.__name__
+        },
+        {
+            'func': blur_gaussian,
+            'grey': False,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': blur_gaussian.__name__
+        },
+        {
+            'func': median,
+            'grey': False,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': median.__name__
+        },
+        {
+            'func': rotate,
+            'grey': False,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': rotate.__name__
+        },
+        {
+            'func': move,
+            'grey': False,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': move.__name__
+        },
+        {
+            'func': contour,
+            'grey': False,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': contour.__name__
+        },
+        {
+            'func': search_geometry,
+            'grey': True,
+            'file_name': FILE_NAME_GEOMETRY,
+            'func_name': search_geometry.__name__
+        },
+        {
+            'func': equalize_hist,
+            'grey': True,
+            'file_name': FILE_NAME_KITTEN,
+            'func_name': equalize_hist.__name__
+        },
+
+    ]
+    for func in functions:
+        @read_write_file(
+            func_name=func['func_name'],
+            grey=func['grey'],
+            file_name=func['file_name']
+        )
+        def call_func(img):
+            f = func['func']
+            return f(img)
+        call_func()
+
